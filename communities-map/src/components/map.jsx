37e29864b2593adb096135ccbe057b5e40ef9481
paolitacute <paolacuevas146@gmail.com>
2026-09-09 14:06:27 -0400

    Update map.jsx

--- communities-map/src/components/map.jsx
+++ communities-map/src/components/map.jsx
@@ -589,7 +589,7 @@ export default function Map() {
       style: maptilersdk.MapStyle.SATELLITE_V4,
       center: [0, 20],
       projection: "globe",
-      zoom: 2.4
+      zoom: 2.1
     });
 
     const markersToUpdate = [];
